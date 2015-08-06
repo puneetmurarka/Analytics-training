08da083956a8c3827f830024d4899bacdfce1b40
puneetmurarka <puneet.murarka.iitkgp@gmail.com>
2015-08-06 17:48:17 +0530

    Update DataStats_draft.R

--- DataStats_draft.R
+++ DataStats_draft.R
@@ -1,3 +1,4 @@
+#You made the first push
 convert.magic <- function(obj,types){
   out <- lapply(1:length(obj),FUN = function(i){FUN1 <- switch(types[i],character = as.character,numeric = as.numeric,factor = as.factor); FUN1(obj[,i])})
   names(out) <- colnames(obj)
